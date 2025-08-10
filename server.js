ccc63f228b8f5e52f383d5bf82652920b191575c
thejaAshwin62 <thejaashwin62@gmail.com>
2025-08-10 10:57:14 +0530

    cors added

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import morgan from "morgan";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import connectDB from "./config/mongoose/database.js";
 import apiRoutes from "./routes/geminiRoutes.js";
 import imageRoutes from "./routes/imageRoutes.js";
 import codeRoutes from "./routes/codeRoutes.js";
@@ -17,10 +16,19 @@ dotenv.config();
 // Express App Setup
 const app = express();
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+app.use(express.json());
 
 // Middleware
-app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(morgan("dev"));
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
